16c59569f11723f4c08d00de836d0f9079124d9b
DirackYellow <noowaayt@gmail.com>
2023-03-14 22:03:14 +0500

    Update srpt.py

--- srpt.py
+++ srpt.py
@@ -1,16 +1,16 @@
 import string
 from random import *
-#необходимо добавить пакет transliterate
+#need to add transliterate package
 from transliterate import translit
-#здесь путь к файлу из которого берутся данные(ФИО)
-#в моем случае путь выглядит так
+#here is the path to the file from which the data is taken (full name)
+    #in my case the path looks like this
 with open('C:\\Users\\79505\\Desktop\\Файл_ФИО.txt', 'r', encoding='utf-8') as file:
     num = [i for i in string.ascii_uppercase if i not in 'OI']
     low = [i for i in string.ascii_lowercase if i not in 'ol']
     upp = [i for i in string.digits[2:]]
     sym = "_?!@#$%"
     symbols = set()
-    # m- длина пароля(в данном случае 8)
+    # m- password length (in this case 8)
     n, m = 1, 8
     while len(symbols) != 56:
         excluded = [73, 79, 91, 92, 93, 94, 95, 96, 108, 111]
@@ -20,7 +20,7 @@ with open('C:\\Users\\79505\\Desktop\\Файл_ФИО.txt', 'r', encoding='utf-8
             continue
         else:
             symbols.add(chr(symbol))
-    #здесь файл в который сохраняются логин-пароль...
+    #here is the file where the login-password is saved...
     with open('C:\\Users\\79505\\Desktop\\лог_пор.txt', 'w', encoding='utf-8') as file_1:
         ru_text = [line.strip().lower() for line in file.readlines()]
         ru_id = []
